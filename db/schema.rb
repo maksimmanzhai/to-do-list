ab7309bc93652b93772d21f2bcbca7f706a112cc
maksimmanzhai <bender23max@gmail.com>
2019-09-01 17:18:10 +0300

    Adding position field

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_135956) do
+ActiveRecord::Schema.define(version: 2019_09_01_141708) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "position"
   end
 
 end
